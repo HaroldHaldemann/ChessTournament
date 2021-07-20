d3a0a55857e1e2bb437aea0212039fcd9becd5f8
Harold Haldemann <harold.haldemann@gmail.com>
2021-07-20 22:52:44 +0200

    feat: add export view

--- Views/main_menu_view.py
+++ Views/main_menu_view.py
@@ -1,10 +1,25 @@
+import exports_view
+import tournaments_view
+
 def main_menu():
 	response = ""
-	while response not in range(5):
-		print(" \nVeuillez selectionner l'action à réaliser en entrant le numéro correspondant:")
+	while response not in [f"{i}" for i in range(5)]:
+		print("\nMENU PRINCIPAL")
+		print(" Veuillez selectionner l'action à réaliser en entrant le numéro correspondant:")
 		print("1- Créer un nouveau tournoi")
 		print("2- Charger un tournoi existant")
 		print("3- Générer un rapport")
 		print("4- Quitter le programme\n")
-		response = input("Veuillez entrer un numéro correspondant à une action et presser entrée")
+		response = input("Veuillez entrer un numéro correspondant à une action et presser entrée\n")
+		if response not in [f"{i}" for i in range(5)]:
+			print("\nCommande incorrect, veuillez entrer un numéro valide")
+	options = {
+		'1': tournaments_view.new_tournament,
+		'2': tournaments_view.load_tournament_menu,
+		'3': exports_view.export_menu,
+		'4': exit,
+	}
+	options[response]()
 
+if __name__ == '__main__':
+	main_menu()
---
+++ Views/tournaments_view.py
@@ -0,0 +1,5 @@
+def new_tournament():
+	pass
+
+def load_tournament_menu():
+	pass
