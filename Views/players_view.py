8ba4e1c754f5bcf310a0fbe98526079351debbcc
Harold Haldemann <harold.haldemann@gmail.com>
2021-08-02 10:54:02 +0200

    feat: add skelettons funtions in views

---
+++ Views/players_view.py
@@ -0,0 +1,7 @@
+class PlayerView:
+
+	def export_all_players():
+		pass
+
+	def export_players_tournament():
+		pass
--- Views/tournaments_view.py
+++ Views/tournaments_view.py
@@ -1,11 +1,17 @@
-import Controllers as controller
+class TournamentView():
 
-def main_menu_view():
-	print("\nMENU PRINCIPAL")
-	print(" Veuillez selectionner l'action à réaliser en entrant le numéro correspondant:")
-	print("1- Créer un nouveau tournoi")
-	print("2- Charger un tournoi existant")
-	print("3- Générer un rapport")
-	print("4- Quitter le programme\n")
-	response = input("Veuillez entrer un numéro correspondant à une action et presser entrée\n")
-	controller.mmc.main_menu_controller(response)
+	def new_tournament():
+		pass
+
+	def load_tournament_menu():
+		pass
+
+	def export_all_tournaments():
+		pass
+
+	def export_tours_tournament():
+		pass
+
+	def export_matches_tournament():
+		pass
+
