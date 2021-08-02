005bc83735fa3d6a1424764b485d91b4226b8e5a
Harold Haldemann <harold.haldemann@gmail.com>
2021-08-02 14:43:09 +0200

    feat: correct controllers conforming to prevuious commit

---
+++ Controllers/menus_controller.py
@@ -0,0 +1,36 @@
+from Views import menus_view
+from Views import tournaments_view
+from Views import players_view
+
+class MenuController():
+
+	def main_menu(self, response):
+		menu_view = menus_view.MenuView()
+		if response not in [f"{i}" for i in range(5)]:
+			print("\nCommande incorrect, veuillez entrer un numéro valide")
+			menu_view.main_menu()
+		tournament_view = tournaments_view.TournamentView()
+		options = {
+			'1': tournament_view.new_tournament,
+			'2': tournament_view.load_tournament,
+			'3': menu_view.export_menu,
+			'4': exit,
+		}
+		options[response]()
+
+	def export_menu(self, response):
+		menu_view = menus_view.MenuView()
+		if response not in [f"{i}" for i in range(7)]:
+			print("\nCommande incorrect, veuillez entrer un numéro valide")
+			menu_view.main_menu()
+		tournament_view = tournaments_view.TournamentView()
+		player_view = players_view.PlayerView()
+		options = {
+			'1': player_view.export_all_players,
+			'2': tournament_view.export_players,
+			'3': tournament_view.export_all_tournaments,
+			'4': tournament_view.export_tours,
+			'5': tournament_view.export_matches,
+			'6': menu_view.main_menu,
+		}
+		options[response]()
