dc8a3c2004b433445b54adf940f3cc269f1e71e6
Harold Haldemann <harold.haldemann@gmail.com>
2021-08-13 12:12:53 +0200

    fix: modify names in views (according to previous commit)

--- Views/menus_view.py
+++ Views/menus_view.py
@@ -4,24 +4,22 @@ class MenuView():
 
 	def main_menu(self):
 		print("\nMENU PRINCIPAL")
-		print("Veuillez selectionner l'action à réaliser en entrant le numéro correspondant:")
 		print("1- Créer un nouveau tournoi")
 		print("2- Charger un tournoi existant")
 		print("3- Générer un rapport")
 		print("4- Quitter le programme\n")
-		response = input("Veuillez entrer un numéro correspondant à une action et presser entrée\n")
+		response = input("Veuillez sélectionner une des options disponibles en entrant son numéro\n")
 		menu_controller = menus_controller.MenuController()
 		menu_controller.main_menu(response)
 
 	def export_menu(self):
 		print("\nMENU GENERATION DE RAPPORT")
-		print("Veuillez selectionner l'action à réaliser en entrant le numéro correspondant:")
 		print("1- Liste de tous les acteurs")
 		print("2- Liste de tous les joueurs d'un tournoi")
 		print("3- Liste de tous les tournois")
 		print("4- Liste de tous les tours d'un tournoi")
 		print("5- Liste de tous les matchs d'un tournoi")
 		print("6- Revenir au menu principal\n")
-		response = input("Veuillez entrer un numéro correspondant à une action et presser entrée\n")
+		response = input("Veuillez sélectionner une des options disponibles en entrant son numéro\n")
 		menu_controller = menus_controller.MenuController()
 		menu_controller.export_menu(response)
--- Views/tournaments_view.py
+++ Views/tournaments_view.py
@@ -2,39 +2,50 @@ from Controllers import tournaments_controller
 
 class TournamentView():
 
-	def new_tournament(self, step):
+	def new_tournament(self, step, args={}):
 		tournament_controller = tournaments_controller.TournamentController()
-		while step < 7:
+		while step < 8:
 			if step == 0:
 				print("\nCREATION D'UN NOUVEAU TOURNOI")
 				step += 1
 			elif step == 1:
 				name = input("\nVeuillez renseigner le nom de ce tournoi\n")
-				tournament_controller.check_args(step, name=name)
+				tournament_controller.check_args(step, args, name=name)
 			elif step == 2:
 				place = input("\nVeuillez renseigner le lieu de ce tournoi\n")
-				tournament_controller.check_args(step, place=place)
+				tournament_controller.check_args(step, args, place=place)
 			elif step == 3:
 				print("\nVeuillez renseigner la date de ce tournoi")
 				date = input("Format: JJ/DD/AAAA\n")
-				tournament_controller.check_args(step, date=date)
+				tournament_controller.check_args(step, args, date=date)
 			elif step == 4:
-				print("\nVeuillez selectionner une des options disponibles")
-				print("1 - Bullet")
-				print("2 - Blitz")
-				print("3 - Coup rapide")
-				time_control = input("Veuillez renseigner le numéro correspondant à vorte choix\n")
-				tournament_controller.check_args(step, time_control=time_control)
+				print("\nVeuillez renseigner votre controle du temps")
+				print("1- Bullet")
+				print("2- Blitz")
+				print("3- Coup rapide")
+				time_control = input("Veuillez sélectionner une des options disponibles en entrant son numéro\n")
+				tournament_controller.check_args(step, args, time_control=time_control)
 			elif step == 5:
 				print("\nVous pouvez renseigner des remarques pour ce tournoi")
 				description = input("Laissez vide dans le cas contraire\n")
-				tournament_controller.check_args(step, description=description)
+				tournament_controller.check_args(step, args, description=description)
 			elif step == 6:
 				print("\nVeuillez renseigner le nombre de tours de ce tournoi")
 				number_rounds = input("Nombre entre 1 et 7 compris (Par défault: 4)\n") or 4
-				tournament_controller.check_args(step, number_rounds=number_rounds)
-		print("OK")
-		exit()
+				tournament_controller.check_args(step, args, number_rounds=number_rounds)
+			elif step == 7:
+				print("\nRESUME DU TOURNOI")
+				print(f"Nom: {args['name']}")
+				print(f"Lieu: {args['place']}")
+				print(f"Date: {args['date']}")
+				print(f"Gestion du temps: {args['time_control']}")
+				print(f"Remarques: {args['description']}")
+				print(f"Nombre de tours: {args['number_rounds']}")
+				print("\nVoulez-vous continuer avec ces paramètres?")
+				print("1- Modifer")
+				print("2- Sauvegarder et continuer")
+				response = input("Veuillez sélectionner une des options disponibles en entrant son numéro\n")
+				tournament_controller.check_args(step, args, response=response)
 
 
 	def load_tournament():
