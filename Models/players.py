e79c3faafb56ca1c18a88af11d7f6d6e1622f9c8
Harold Haldemann <harold.haldemann@gmail.com>
2021-08-14 10:36:59 +0200

    fix: fix add player to db

--- Models/players.py
+++ Models/players.py
@@ -8,11 +8,11 @@ class Player():
 
 	def add_player_to_db(self, last_name, first_name, birth_date, gender, ranking):
 		serialized_player = {
-			'first_name': player.first_name,
-			'last_name': player.last_name,
-			'birth_date': player.birth_date,
-			'gender': player.gender,
-			'ranking': player.ranking,
+			'last_name': last_name,
+			'first_name': first_name,
+			'birth_date': birth_date,
+			'gender': gender,
+			'ranking': ranking,
 		}
 		self.TABLE.insert(serialized_player)
 
@@ -20,17 +20,15 @@ class Player():
 	def get_all_players(self):
 		return sorted(
 			self.TABLE.all(), 
-			key=itemgetter('last_name', 'first_name', 'birth_date', 'ranking'),
+			key=itemgetter('last_name', 'first_name', 'birth_date'),
 		)
 
 
-	def get_player(self, last_name, first_name, birth_date, gender, ranking):
+	def get_player(self, last_name, first_name, birth_date):
 		Player = Query()
 		player = self.TABLE.search(
 			Player['last_name'] == last_name and \
 			Player['first_name'] == first_name and \
-			Player['birth_date'] == birth_date and \
-			Player['gender'] == gender and \
-			Player['ranking'] == ranking
+			Player['birth_date'] == birth_date
 		)
 		return player
