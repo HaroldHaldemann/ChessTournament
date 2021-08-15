31e1d133b7f3e50ee9db28da4ce5a310cbf34862
Harold Haldemann <harold.haldemann@gmail.com>
2021-08-15 19:34:41 +0200

    feat: addplayer to db and to tournament functions

--- Controllers/tournaments_controller.py
+++ Controllers/tournaments_controller.py
@@ -17,17 +17,19 @@ class TournamentController():
 			args['number_rounds']
 		)
 		player = players.Player()
-		all_players = player.get_all_players()
+		all_players = player.get_all_players()	
 		if len(all_players) < 8:
 			print("\nATTENTION")
 			print("Le nombre de joueur est insuffisant pour un tournoi")
 			print("Vous allez être redirigé vers le menu d'ajout de joueur")
 			print("Vous pourrez charger ce tournoi à partir du menu principal")
 			player_view = players_view.PlayerView()
-			player_view.add_player_to_db()
+			args = {}
+			player_view.add_player_to_db(args)
 		tournament_view = tournaments_view.TournamentView()
-		tournament_view.add_player_to_tournament(all_players)
-
+		list_players = []
+		tournament_view.add_player_to_tournament(all_players, list_players)
+		
 
 	def add_player_to_tournament(self, all_players, players):
 		input_player = players[-1]
@@ -35,17 +37,53 @@ class TournamentController():
 		players.pop()
 		if input_player == '0':
 			player_view = players_view.PlayerView()
-			player_view.add_player_to_db()
+			args = {}
+			player_view.add_player_to_db(args)
 		if self.check_player(all_players, input_player):
-			player = all_players[int(input_player)]
+			player = all_players[int(input_player)-1]
 			players.append(player)
+			all_players.pop(int(input_player)-1)
+		if len(players) == 8:
+			pass # TODO - Add round to tournament
 		tournament_view.add_player_to_tournament(all_players, players)
+
+
+	def load_tournament(self, list_tournaments=[], response="", finished=""):
+		tournament = tournaments.Tournament()
+		tournament_view = tournaments_view.TournamentView()
+		if finished:
+			options = {
+				'1': tournament.get_unfinished_tournaments,
+				'2': tournament.get_finished_tournaments,
+			}
+			list_tournaments = options[finished]()
+			response = ""
+			tournament_view.load_tournament(list_tournaments)
+		if response == '0':
+			args = {}
+			tournament_view.new_tournament(args)
+		if self.check_response_load(response, list_tournaments):
+			loaded_tournament = list_tournaments[int(response)-1]
+			if not loaded_tournament['finished']:
+				if not loaded_tournament['players']:
+					tournament_view.new_tournament({
+						'name': loaded_tournament['name'],
+						'place': loaded_tournament['place'],
+						'date': loaded_tournament['date'],
+						'time_control': loaded_tournament['time_control'],
+						'description': loaded_tournament['description'],
+						'number_rounds': loaded_tournament['number_rounds'],
+					})
+				else:
+					pass # TODO - Add round to tournament
+
 
 
 ###############################################################
 #  UTILS                                                      #
 ###############################################################
 
+### NEW TOURNAMENT ###
 	
 	def check_args(self, args, **kwargs):
 		key = list(kwargs.keys())[0]
@@ -53,21 +91,24 @@ class TournamentController():
 		util = utils.Util()
 		value = util.input_format(value)
 		tournament_view = tournaments_view.TournamentView()
-		option = {
+		options = {
 			'name': self.check_name,
 			'place': self.check_place,
 			'date': util.check_date,
 			'time_control': self.check_time_control,
 			'description': self.check_description,
 			'number_rounds': self.check_number_rounds,
-			'response': self.check_response,
+			'response': self.check_response_new,
 		}
-		if option[key](value):
-			if key == 'response':
-				if value == '1':
-					tournament_view.new_tournament()
-			value = option[key](value)
+		if options[key](value):
+			value = options[key](value)
 			args[key] = value
+			if key == 'response':
+				tournament = tournaments.Tournament()
+				tournament.remove_tournament_from_db(args['name'])
+				if value == '1':
+					args = {}
+					tournament_view.new_tournament(args)
 		tournament_view.new_tournament(args)
 
 
@@ -93,12 +134,12 @@ class TournamentController():
 		if time_control not in ['1', '2', '3']:
 			print("Gestion de temps invalide")
 			return False
-		option = {
+		options = {
 			'1': "bullet",
 			'2': "blitz",
 			'3': "coup rapide",
 		}
-		return option[time_control]
+		return options[time_control]
 
 	def check_description(self, description):
 		if description == "":
@@ -117,15 +158,29 @@ class TournamentController():
 		return number_rounds
 
 
-	def check_response(self, response):
+	def check_response_new(self, response):
 		if response not in ['1', '2']:
 			print("Réponse invalide")
 			return False
 		return response
 
 
+### ADD PLAYER TO TOURNAMENT ###
+
+
 	def check_player(self, all_players, input_player):
-		if input_player not in [f"{index}" for index in range(1, len(all_players) + 1)]:
+		if input_player not in [f"{index+1}" for index in range(len(all_players))]:
 			print("Joueur invalide")
 			return False
 		return True
+
+
+### LOAD TOURNAMENT ###
+
+
+	def check_response_load(self, response, list_tournaments):
+		if response not in [f"{index+1}" for index in range(len(list_tournaments))]:
+			print("Réponse invalide")
+			return False
+		return True
+			
