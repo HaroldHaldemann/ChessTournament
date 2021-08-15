2956724751b95737f9ab0631c72562ebd5b9afc0
Harold Haldemann <harold.haldemann@gmail.com>
2021-08-15 19:34:08 +0200

    fix: problem with args variables

--- Controllers/menus_controller.py
+++ Controllers/menus_controller.py
@@ -1,19 +1,21 @@
 from Views import menus_view
 from Views import tournaments_view
 from Views import players_view
+from . import tournaments_controller
 
 class MenuController():
 
 	def main_menu(self, response):
 		menu_view = menus_view.MenuView()
-		if response not in [f"{i}" for i in range(5)]:
+		if response not in [f"{i+1}" for i in range(5)]:
 			print("\nCommande incorrect, veuillez entrer un numéro valide")
 			menu_view.main_menu()
 		tournament_view = tournaments_view.TournamentView()
+		tournament_controller = tournaments_controller.TournamentController()
 		player_view = players_view.PlayerView()
 		options = {
 			'1': tournament_view.new_tournament,
-			'2': tournament_view.load_tournament,
+			'2': menu_view.load_menu,
 			'3': player_view.add_player_to_db,
 			'4': menu_view.export_menu,
 			'5': exit,
@@ -22,7 +24,7 @@ class MenuController():
 
 	def export_menu(self, response):
 		menu_view = menus_view.MenuView()
-		if response not in [f"{i}" for i in range(7)]:
+		if response not in [f"{i+1}" for i in range(7)]:
 			print("\nCommande incorrect, veuillez entrer un numéro valide")
 			menu_view.main_menu()
 		tournament_view = tournaments_view.TournamentView()
@@ -36,3 +38,19 @@ class MenuController():
 			'6': menu_view.main_menu,
 		}
 		options[response]()
+
+	def load_menu(self, response):
+		menu_view = menus_view.MenuView()
+		if response not in [f"{i+1}" for i in range(3)]:
+			print("\nCommande incorrect, veuillez entrer un numéro valide")
+			menu_view.main_menu()
+		tournament_controller = tournaments_controller.TournamentController()
+		options = {
+			'1': [tournament_controller.load_tournament, response],
+			'2': [tournament_controller.load_tournament, response],
+			'3': [menu_view.main_menu, None],
+		}
+		args = options[response][1]
+		if args:
+			options[response][0](finished=args)
+		options[response][0]()
