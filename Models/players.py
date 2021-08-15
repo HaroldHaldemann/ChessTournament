2aa9a90d8332421ac52c728d03facdd1a26f1126
Harold Haldemann <harold.haldemann@gmail.com>
2021-08-15 19:32:43 +0200

    feat: remove init and str, add remove and get player

--- Models/players.py
+++ Models/players.py
@@ -23,12 +23,21 @@ class Player():
 			key=itemgetter('last_name', 'first_name', 'birth_date'),
 		)
 
+	def remove_player_from_db(self, last_name, first_name, birth_date):
+		Playert = Query()
+		if self.get_player(last_name, first_name, birth_date):
+			self.TABLE.remove(
+				(Player['last_name'] == last_name) & \
+				(Player['first_name'] == first_name) & \
+				(Player['birth_date'] == birth_date)
+			)
 
+	
 	def get_player(self, last_name, first_name, birth_date):
 		Player = Query()
 		player = self.TABLE.search(
-			Player['last_name'] == last_name and \
-			Player['first_name'] == first_name and \
-			Player['birth_date'] == birth_date
+			(Player['last_name'] == last_name) & \
+			(Player['first_name'] == first_name) & \
+			(Player['birth_date'] == birth_date)
 		)
 		return player
