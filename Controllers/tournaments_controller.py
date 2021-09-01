aef01968afe6de641dbd492a7c016ad08fc86fad
Harold Haldemann <harold.haldemann@gmail.com>
2021-09-01 14:42:42 +0200

    feat: add decorators, imporved imports, refactoring, add confirm player

--- Controllers/tournaments_controller.py
+++ Controllers/tournaments_controller.py
@@ -1,72 +1,79 @@
-from Views import tournaments_view
-from Views import players_view
-from Models import tournaments
-from Models import players
-from . import utils
+import Views
+import Models
+from .utils import Util
 
 class TournamentController():
 
-	def new_tournament(self, args):
-		tournament = tournaments.Tournament()
-		tournament.add_tournament_to_db(
+	@staticmethod
+	def new_tournament(args):
+		tournament = Models.Tournament(
 			args['name'], 
 			args['place'], 
 			args['date'], 
 			args['time_control'], 
 			args['description'], 
-			args['number_rounds']
+			args['number_rounds'],
+			[],
+			[],
 		)
-		player = players.Player()
-		all_players = player.get_all_players()	
+		tournament.remove_from_db()
+		tournament.add_to_db()
+
+		all_players = Models.Player.get_all_players()	
+
 		if len(all_players) < 8:
 			print("\nATTENTION")
 			print("Le nombre de joueur est insuffisant pour un tournoi")
-			print("Vous allez être redirigé vers le menu d'ajout de joueur")
+			print("Vous allez être redirigé(e) vers le menu d'ajout de joueur")
 			print("Vous pourrez charger ce tournoi à partir du menu principal")
-			player_view = players_view.PlayerView()
+			
 			args = {}
-			player_view.add_player_to_db(args)
-		tournament_view = tournaments_view.TournamentView()
+			Views.PlayerView.add_player_to_db(args)
+
 		list_players = []
-		tournament_view.add_player_to_tournament(all_players, list_players)
+		Views.TournamentView.add_player_to_tournament(all_players, list_players)
 		
 
-	def add_player_to_tournament(self, all_players, players):
+	@classmethod
+	def add_player_to_tournament(cls, all_players, players):
 		input_player = players[-1]
-		tournament_view = tournaments_view.TournamentView()
 		players.pop()
+
 		if input_player == '0':
-			player_view = players_view.PlayerView()
 			args = {}
-			player_view.add_player_to_db(args)
-		if self.check_player(all_players, input_player):
+			Views.PlayerView.add_player_to_db(args)
+
+		if cls.check_player(all_players, input_player):
 			player = all_players[int(input_player)-1]
 			players.append(player)
 			all_players.pop(int(input_player)-1)
+
 		if len(players) == 8:
-			pass # TODO - Add round to tournament
-		tournament_view.add_player_to_tournament(all_players, players)
+			Views.TournamentView.confirm_players(all_players, players)
+
+		Views.TournamentView.add_player_to_tournament(all_players, players)
+
+	@classmethod
+	def confirm_players(cls, all_players, players, response):
+		pass
 
 
-	def load_tournament(self, list_tournaments=[], response="", finished=""):
-		tournament = tournaments.Tournament()
-		tournament_view = tournaments_view.TournamentView()
-		if finished:
-			options = {
-				'1': tournament.get_unfinished_tournaments,
-				'2': tournament.get_finished_tournaments,
-			}
-			list_tournaments = options[finished]()
-			response = ""
-			tournament_view.load_tournament(list_tournaments)
-		if response == '0':
-			args = {}
-			tournament_view.new_tournament(args)
-		if self.check_response_load(response, list_tournaments):
-			loaded_tournament = list_tournaments[int(response)-1]
+	@classmethod
+	def load_tournament(cls, list_tournaments, response):
+		args = {}
+		options = {
+			'0': Views.TournamentView.new_tournament(args),
+		}
+		for index in range(len(list_tournaments)):
+			options[f"{index + 1}"] = list_tournaments[index]
+
+		if Util.check_response(response, options) or response != '0':
+			loaded_tournament = options[response]
+
 			if not loaded_tournament['finished']:
+
 				if not loaded_tournament['players']:
-					tournament_view.new_tournament({
+					Views.TournamentView.new_tournament({
 						'name': loaded_tournament['name'],
 						'place': loaded_tournament['place'],
 						'date': loaded_tournament['date'],
@@ -78,62 +85,83 @@ class TournamentController():
 					pass # TODO - Add round to tournament
 
 
+	def add_round_to_tournament():
+		pass
 
-###############################################################
-#  UTILS                                                      #
-###############################################################
 
-### NEW TOURNAMENT ###
+
+#############################################
+# 									UTILS 									#
+#############################################
+
+
+##### NEW TOURNAMENT #####
+
 	
-	def check_args(self, args, **kwargs):
+	@classmethod
+	def check_args(cls, args, **kwargs):
 		key = list(kwargs.keys())[0]
 		value = list(kwargs.values())[0]
-		util = utils.Util()
-		value = util.input_format(value)
-		tournament_view = tournaments_view.TournamentView()
+		value = Util.input_format(value)
+
 		options = {
-			'name': self.check_name,
-			'place': self.check_place,
-			'date': util.check_date,
-			'time_control': self.check_time_control,
-			'description': self.check_description,
-			'number_rounds': self.check_number_rounds,
-			'response': self.check_response_new,
+			'name': cls.check_name,
+			'place': cls.check_place,
+			'date': Util.check_date,
+			'time_control': cls.check_time_control,
+			'description': cls.check_description,
+			'number_rounds': cls.check_number_rounds,
+			'response': [Util.check_response, 2],
 		}
-		if options[key](value):
-			value = options[key](value)
+		value = Util.call_option(options, key, value)
+		if key == 'response':
+
+		value = options[key](value)
+
+		if value:
 			args[key] = value
+
 			if key == 'response':
-				tournament = tournaments.Tournament()
-				tournament.remove_tournament_from_db(args['name'])
+					options = {
+						'1': ,
+						'2': ,
+					}
 				if value == '1':
 					args = {}
-					tournament_view.new_tournament(args)
-		tournament_view.new_tournament(args)
+				else:
+					cls.new_tournament(args)
+
+		Views.TournamentView.new_tournament(args)
 
 
-	def check_name(self, name):
+	@staticmethod
+	def check_name(name):
 		if name == "":
 			print("Nom invalide: entrée vide")
 			return False
-		tournament = tournaments.Tournament()
-		if name in tournament.get_name_tournaments():
+
+		if name in Models.Tournament.get_name_tournaments():
 			print("Nom invalide: nom déjà existant")
 			return False
+
 		return name
 
 
-	def check_place(self, place):
+	@staticmethod
+	def check_place(place):
 		if place == "":
 			print("Lieu invalide: entrée vide")
 			return False
+
 		return place
 
 
-	def check_time_control(self, time_control):
+	@staticmethod
+	def check_time_control(time_control):
 		if time_control not in ['1', '2', '3']:
 			print("Gestion de temps invalide")
 			return False
+
 		options = {
 			'1': "bullet",
 			'2': "blitz",
@@ -141,46 +169,39 @@ class TournamentController():
 		}
 		return options[time_control]
 
-	def check_description(self, description):
+
+	@staticmethod
+	def check_description(description):
 		if description == "":
-			description = " "
+			description = "Aucun commentaire"
+
 		return description
 
-	def check_number_rounds(self, number_rounds):
+
+	@staticmethod
+	def check_number_rounds(number_rounds):
 		if type(number_rounds) == int:
 			return number_rounds
+
 		if not number_rounds.isdigit():
 			print("Nombre de tours invalide")
 			return False
+
 		if not (0 < int(number_rounds) < 8):
 			print("Nombre de tours invalide")
 			return False
+
 		return number_rounds
 
 
-	def check_response_new(self, response):
-		if response not in ['1', '2']:
-			print("Réponse invalide")
-			return False
-		return response
+##### ADD PLAYER TO TOURNAMENT #####
 
 
-### ADD PLAYER TO TOURNAMENT ###
-
-
-	def check_player(self, all_players, input_player):
+	@staticmethod
+	def check_player(all_players, input_player):
 		if input_player not in [f"{index+1}" for index in range(len(all_players))]:
 			print("Joueur invalide")
 			return False
-		return True
 
-
-### LOAD TOURNAMENT ###
-
-
-	def check_response_load(self, response, list_tournaments):
-		if response not in [f"{index+1}" for index in range(len(list_tournaments))]:
-			print("Réponse invalide")
-			return False
-		return True
+		return input_player
 			
