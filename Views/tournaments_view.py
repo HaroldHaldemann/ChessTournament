9737323a2a1c1133758c127540ffaa37abd1c524
Harold Haldemann <harold.haldemann@gmail.com>
2021-09-01 12:17:48 +0200

    feat: add decorators and improved  imports, refactoring, add confirm_player view

--- Views/tournaments_view.py
+++ Views/tournaments_view.py
@@ -1,73 +1,144 @@
-from Controllers import tournaments_controller
+import Controllers
 
 class TournamentView():
 
-	def new_tournament(self, args={}):
-		step = 7-len(args)
-		tournament_controller = tournaments_controller.TournamentController()
-		while step != 0:
-			print("\nCREATION D'UN NOUVEAU TOURNOI")
-			if step == 7:
-				name = input("Veuillez renseigner le nom de ce tournoi\n")
-				tournament_controller.check_args(args, name=name)
-			elif step == 6:
-				place = input("Veuillez renseigner le lieu de ce tournoi\n")
-				tournament_controller.check_args(args, place=place)
-			elif step == 5:
-				print("Veuillez renseigner la date de ce tournoi")
-				date = input("Format: JJ/MM/AAAA\n")
-				tournament_controller.check_args(args, date=date)
-			elif step == 4:
-				print("Veuillez renseigner votre controle du temps")
-				print("1- Bullet")
-				print("2- Blitz")
-				print("3- Coup rapide")
-				time_control = input("Veuillez sélectionner une des options disponibles en entrant son numéro\n")
-				tournament_controller.check_args(args, time_control=time_control)
-			elif step == 3:
-				print("Vous pouvez renseigner des remarques pour ce tournoi")
-				description = input("Laissez vide dans le cas contraire\n")
-				tournament_controller.check_args(args, description=description)
-			elif step == 2:
-				print("Veuillez renseigner le nombre de tours de ce tournoi")
-				number_rounds = input("Nombre entre 1 et 7 compris (Par défault: 4)\n") or 4
-				tournament_controller.check_args(args, number_rounds=number_rounds)
-			elif step == 1:
-				print("Résumé du tournoi")
-				print(f"Nom: {args['name']}")
-				print(f"Lieu: {args['place']}")
-				print(f"Date: {args['date']}")
-				print(f"Gestion du temps: {args['time_control']}")
-				print(f"Remarques: {args['description']}")
-				print(f"Nombre de tours: {args['number_rounds']}")
-				print("\nVoulez-vous continuer avec ces paramètres?")
-				print("1- Modifer")
-				print("2- Sauvegarder et continuer")
-				response = input("Veuillez sélectionner une des options disponibles en entrant son numéro\n")
-				tournament_controller.check_args(args, response=response)
-		tournament_controller.new_tournament(args)
+##### NEW TOURNAMENT #####
+
+	@classmethod
+	def new_tournament(cls, args={}):
+		step = len(args)
+		print("\nCREATION D'UN NOUVEAU TOURNOI")
+
+		options = [
+			cls.new_tournament_name,
+			cls.new_tournament_place,
+			cls.new_tournament_date,
+			cls.new_tournament_time_control,
+			cls.new_tournament_description,
+			cls.new_tournament_number_rounds,
+			cls.new_tournament_response,
+		]
+		options[step](args)
 
 
-	def add_player_to_tournament(self, all_players, players=[]):
+	@staticmethod
+	def new_tournament_name(args):
+		name = input("Veuillez renseigner le nom de ce tournoi\n")
+
+		Controllers.TournamentController.check_args(args, name=name)
+
+
+	@staticmethod
+	def new_tournament_place(args):
+		place = input("Veuillez renseigner le lieu de ce tournoi\n")
+
+		Controllers.TournamentController.check_args(args, place=place)
+
+
+	@staticmethod
+	def new_tournament_date(args):
+		print("Veuillez renseigner la date de ce tournoi")
+		date = input("Format: AAAA-MM-JJ\n")
+
+		Controllers.TournamentController.check_args(args, date=date)
+
+
+	@staticmethod
+	def new_tournament_time_control(args):
+		print("Veuillez renseigner votre controle du temps")
+		print("1- Bullet")
+		print("2- Blitz")
+		print("3- Coup rapide")
+
+		time_control = input("Veuillez sélectionner une des options disponibles en entrant son numéro\n")
+
+		Controllers.TournamentController.check_args(args, time_control=time_control)
+
+
+	@staticmethod
+	def new_tournament_description(args):
+		print("Vous pouvez renseigner des remarques pour ce tournoi")
+		description = input("Laissez vide dans le cas contraire\n")
+
+		Controllers.TournamentController.check_args(args, description=description)
+
+
+	@staticmethod
+	def new_tournament_number_rounds(args):
+		print("Veuillez renseigner le nombre de tours de ce tournoi")
+		number_rounds = input("Nombre entre 1 et 7 compris (Par défault: 4)\n") or 4
+
+		Controllers.TournamentController.check_args(args, number_rounds=number_rounds)
+
+
+	@staticmethod
+	def new_tournament_response(args):
+		print("Résumé du tournoi")
+		print(f"Nom: {args['name']}")
+		print(f"Lieu: {args['place']}")
+		print(f"Date: {args['date']}")
+		print(f"Gestion du temps: {args['time_control']}")
+		print(f"Remarques: {args['description']}")
+		print(f"Nombre de tours: {args['number_rounds']}")
+
+		print("\nVoulez-vous continuer avec ces paramètres?")
+		print("1- Modifer")
+		print("2- Sauvegarder et continuer")
+
+		response = input("Veuillez sélectionner une des options disponibles en entrant son numéro\n")
+		Controllers.TournamentController.check_args(args, response=response)
+
+
+##### ADD PLAYER TO TOURNAMENT #####
+
+
+	@staticmethod
+	def add_player_to_tournament(all_players, players=[]):
 		print("\nAJOUT DE JOUEURS")
 		print(f"Veuillez ajouter {8-len(players)} joueurs dans le tournoi")
+
 		print("\n0- Ajouter de nouveaux joueurs à la base de données")
 		print("\nListe des joueurs:")
+
 		for index, player in enumerate(all_players):
 			print(f"\n{index + 1}- Nom: {player['last_name']} {player['first_name']}")
 			print(f"Date de naissance: {player['birth_date']}")
 			print(f"Genre: {player['gender']}")
 			print(f"Classement: {player['ranking']}")
+			
 		player = input("Veuillez sélectionner une des options disponibles en entrant son numéro\n")
 		players.append(player)
-		tournament_controller = tournaments_controller.TournamentController()
-		tournament_controller.add_player_to_tournament(all_players, players)
+
+		Controllers.TournamentController.add_player_to_tournament(all_players, players)
 
 
-	def load_tournament(self, list_tournaments):
+	@staticmethod
+	def confirm_players(all_players, players):
+		print("\nAJOUT DE JOUEURS")
+		print("\nListe des joueurs:")
+
+		for index, player in enumerate(players):
+			print(f"\n{index + 1}- Nom: {player['last_name']} {player['first_name']}")
+			print(f"Date de naissance: {player['birth_date']}")
+			print(f"Genre: {player['gender']}")
+			print(f"Classement: {player['ranking']}")
+
+		print("\nVoulez-vous ces joueurs pour le tournoi?")
+		print("1- Confimer et sauvegarder")
+		print("2- Modifier la séléction")
+		response = input("Veuillez sélectionner une des options disponibles en entrant son numéro\n")
+		Controllers.TournamentController.confirm_players(all_players, players, response)
+
+
+##### LOAD TOURNAMENT #####
+
+
+	@staticmethod
+	def load_tournament(list_tournaments):
 		print("\nCHARGEMENT D'UN TOURNOI")
 		print("0- Créer un nouveau tournoi")
 		print("Liste des tournois:")
+
 		for index, tournament in enumerate(list_tournaments):
 			print(
 				f"{index+1}- " \
@@ -77,8 +148,12 @@ class TournamentView():
 				f"Gestion du temps: {tournament['time_control']}"
 			)
 		response = input("Veuillez sélectionner une des options disponibles en entrant son numéro\n")
-		tournament_controller = tournaments_controller.TournamentController()
-		tournament_controller.load_tournament(list_tournaments, response=response, finished="")
+
+		Controllers.TournamentController.load_tournament(list_tournaments, response=response)
+
+
+##### EXPORTS #####
+
 
 	def export_players(self):
 		pass
