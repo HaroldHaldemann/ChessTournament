ab4409aee72f7de77f0067f94e4dbfd9933d9fac
Harold Haldemann <harold.haldemann@gmail.com>
2021-09-01 12:21:30 +0200

    feat: add decorators and improved imports, refactoring

--- Controllers/menus_controller.py
+++ Controllers/menus_controller.py
@@ -1,56 +1,59 @@
-from Views import menus_view
-from Views import tournaments_view
-from Views import players_view
-from . import tournaments_controller
+import Views
+import Models
+from .utils import Util
 
 class MenuController():
 
-	def main_menu(self, response):
-		menu_view = menus_view.MenuView()
-		if response not in [f"{i+1}" for i in range(5)]:
-			print("\nCommande incorrect, veuillez entrer un numéro valide")
-			menu_view.main_menu()
-		tournament_view = tournaments_view.TournamentView()
-		tournament_controller = tournaments_controller.TournamentController()
-		player_view = players_view.PlayerView()
+	@staticmethod
+	def main_menu(response):
 		options = {
-			'1': tournament_view.new_tournament,
-			'2': menu_view.load_menu,
-			'3': player_view.add_player_to_db,
-			'4': menu_view.export_menu,
+			'1': Views.TournamentView.new_tournament,
+			'2': Views.MenuView.load_menu,
+			'3': Views.PlayerView.add_player_to_db,
+			'4': Views.MenuView.export_menu,
 			'5': exit,
 		}
+		if Util.check_reponse(response, options):
+			MenuView.main_menu()
+			
 		options[response]()
 
-	def export_menu(self, response):
-		menu_view = menus_view.MenuView()
-		if response not in [f"{i+1}" for i in range(7)]:
-			print("\nCommande incorrect, veuillez entrer un numéro valide")
-			menu_view.main_menu()
-		tournament_view = tournaments_view.TournamentView()
-		player_view = players_view.PlayerView()
+
+	@staticmethod
+	def export_menu(response):
 		options = {
-			'1': player_view.export_all_players,
-			'2': tournament_view.export_players,
-			'3': tournament_view.export_all_tournaments,
-			'4': tournament_view.export_rounds,
-			'5': tournament_view.export_matches,
-			'6': menu_view.main_menu,
+			'1': Views.PlayerView.export_all_players,
+			'2': Views.TournamentView.export_players,
+			'3': Views.TournamentView.export_all_tournaments,
+			'4': Views.TournamentView.export_rounds,
+			'5': Views.TournamentView.export_matches,
+			'6': MenuView.main_menu,
 		}
+		if Util.check_reponse(response, options):
+			MenuView.main_menu()
+
 		options[response]()
 
-	def load_menu(self, response):
-		menu_view = menus_view.MenuView()
-		if response not in [f"{i+1}" for i in range(3)]:
-			print("\nCommande incorrect, veuillez entrer un numéro valide")
-			menu_view.main_menu()
-		tournament_controller = tournaments_controller.TournamentController()
+
+	@staticmethod
+	def load_menu(response):
 		options = {
-			'1': [tournament_controller.load_tournament, response],
-			'2': [tournament_controller.load_tournament, response],
-			'3': [menu_view.main_menu, None],
+			'1': [
+				Views.TournamentView.load_tournament,
+				Models.Tournament.get_unfinished_tournaments(),
+			],
+			'2': [
+				Views.TournamentView.load_tournament,
+				Models.Tournament.get_finished_tournaments(),
+			],
+			'3': [Views.MenuView.main_menu, None],
 		}
+		if Util.check_reponse(response, options):
+			MenuView.main_menu()
+
 		args = options[response][1]
-		if args:
-			options[response][0](finished=args)
-		options[response][0]()
+
+		if args != None:
+			options[response][0](list_tournaments=args)
+		else:
+			options[response][0]()
