a2e476da265e6739777b116a1eb315c24019566b
Harold Haldemann <harold.haldemann@gmail.com>
2021-09-14 15:20:56 +0200

    feat: remove dataclass, add init and lt magic method, use serialized players in functions

--- Models/players.py
+++ Models/players.py
@@ -1,56 +1,87 @@
 from tinydb import TinyDB, Query
 from operator import itemgetter
-from dataclasses import dataclass
-import datetime
 
 DATABASE = TinyDB('database.json')
 TABLE = DATABASE.table('players')
 
-
-@dataclass
 class Player():
 
-	last_name: str
-	first_name: str
-	birth_date: datetime.date
-	gender: str = ""
-	ranking: int = 0
+	def __init__(
+			self,
+			last_name = "",
+			first_name = "",
+			birth_date = "",
+			gender = "",
+			ranking = 0,
+		):
+		self.last_name = last_name
+		self.first_name = first_name
+		self.birth_date = birth_date
+		self.gender = gender
+		self.ranking = ranking
 
-	def add_to_db(self):
-		serialized_player = {
+	def __lt__(self, other):
+		if isinstance(other, Player):
+			return self.ranking < other.ranking
+		return False
+
+	def serialize(self):
+		return {
 			'last_name': self.last_name,
 			'first_name': self.first_name,
 			'birth_date': self.birth_date,
 			'gender': self.gender,
 			'ranking': self.ranking,
 		}
-		TABLE.insert(serialized_player)
-
-
-	def remove_from_db(self):
-		Playert = Query()
-		if self.get_player(last_name, first_name, birth_date):
-			TABLE.remove(
-				(Player['last_name'] == self.last_name) & \
-				(Player['first_name'] == self.first_name) & \
-				(Player['birth_date'] == self.birth_date)
-			)
-
 
 	@staticmethod
-	def get_from_db(self, last_name, first_name, birth_date):
-		Player = Query()
-		player = TABLE.search(
-			(Player['last_name'] == last_name) & \
-			(Player['first_name'] == first_name) & \
-			(Player['birth_date'] == birth_date)
-		)
+	def deserialize(serialized_player):
+		player = Player()
+		player.last_name = serialized_player['last_name']
+		player.first_name = serialized_player['first_name']
+		player.birth_date = serialized_player['birth_date']
+		player.gender = serialized_player['gender']
+		player.ranking = serialized_player['ranking']
 		return player
 
+	def add_to_db(self):
+		if self.get_from_db(self.last_name, self.first_name, self.birth_date):
+			Player = Query()
+			TABLE.update(
+					{'gender': self.gender, 'ranking': self.ranking},
+					(Player.last_name == self.last_name) & \
+					(Player.first_name == self.first_name) & \
+					(Player.birth_date == self.birth_date),
+				)
+		else:
+			TABLE.insert(self.serialize())
 
-	@staticmethod
-	def get_all_players():
-		return sorted(
+	def remove_from_db(self):
+		Player = Query()
+		if self.get_from_db(self.last_name, self.first_name, self.birth_date):
+			TABLE.remove(
+				(Player.last_name == self.last_name) & \
+				(Player.first_name == self.first_name) & \
+				(Player.birth_date == self.birth_date)
+			)
+
+	@classmethod
+	def get_from_db(cls, last_name, first_name, birth_date):
+		Player = Query()
+		serialized_player = TABLE.search(
+			(Player.last_name == last_name) & \
+			(Player.first_name == first_name) & \
+			(Player.birth_date == birth_date)
+		)
+		if serialized_player:
+			return cls.deserialize(serialized_player[0])
+		else:
+			return serialized_player
+
+	@classmethod
+	def get_all_players(cls):
+		serialized_players = sorted(
 			TABLE.all(), 
 			key=itemgetter('last_name', 'first_name', 'birth_date'),
 		)
+		return [cls.deserialize(player) for player in serialized_players]
