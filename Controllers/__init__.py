29ca5e5e6f4661739d6ee3cbfcdad4a871e15415
Harold Haldemann <harold.haldemann@gmail.com>
2021-09-14 15:01:01 +0200

    feat: add roundcontroller in imports

--- Controllers/__init__.py
+++ Controllers/__init__.py
@@ -1,3 +1,5 @@
 from .menus_controller import MenuController
 from .players_controller import PlayerController
 from .tournaments_controller import TournamentController
+from .rounds_controller import RoundController
+from .utils import Util
