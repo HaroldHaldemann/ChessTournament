b8a462739b3687a8c878e6319f5605837ec4071a
Harold Haldemann <harold.haldemann@gmail.com>
2021-09-14 14:59:56 +0200

    feat: replacxe dict with model

--- Controllers/players_controller.py
+++ Controllers/players_controller.py
@@ -4,34 +4,46 @@ from .utils import Util
 
 class PlayerController():
 
-
 	@staticmethod
-	def add_player_to_db(args):
-		player = Models.Player(
-			args['last_name'],
-			args['first_name'],
-			args['birth_date'],
-			args['gender'],
-			args['ranking'],
-		)
-		player.remove_from_db()
+	def add_player_to_db(player, response):
 		player.add_to_db()
 
 		options = {
-			'2': [Views.PlayerView.add_player_to_db, {}],
-			'3': [Views.MenuView.main_menu, None],
+			'1': [Views.PlayerView.add_player_to_db, Models.Player(), 0],
+			'2': Views.MenuView.main_menu,
 		}
-		response = args['response']
 		Util.call_options(options, response)
 
+	@staticmethod
+	def load_player(all_players, response):
+		if response == '0':
+			Views.MenuView.main_menu()
+
+		if not Util.check_player(all_players, response):
+			Views.PlayerView.load_player(all_players)
+
+		player = all_players[int(response)-1]
+		Views.PlayerView.modify_player(all_players, player, response)
+
+	@staticmethod
+	def modify_player(all_players, player, response, input_player):
+		options = {
+			'1': [Views.PlayerView.add_player_to_db, player, 4],
+			'2': player.remove_from_db,
+			'3': Views.MenuView.main_menu,
+		}
+		Util.call_options(options, response)
+		print("Le joueur a bien été supprimé")
+		print("Retour au menu de chargement")
+		all_players.pop(int(input_player)-1)
+		Views.PlayerView.load_player(all_players)
 
 #############################################
 # 									UTILS 									#
 #############################################
 
-	
 	@classmethod
-	def check_args(cls, args, **kwargs):
+	def check_args(cls, player, step, **kwargs):
 		key = list(kwargs.keys())[0]
 		value = list(kwargs.values())[0]
 		value = Util.input_format(value)
@@ -42,29 +54,30 @@ class PlayerController():
 			'birth_date': Util.check_date,
 			'gender': cls.check_gender,
 			'ranking': cls.check_ranking,
-			'response': Util.check_response,
+			'response': [Util.check_response, 3]
 		}
-		value = options[key](value)
+		value = Util.call_options(options, key, value)
 
 		if value:
-			args[key] = value
-
 			if key == 'response':
 				options = {
-					'1': [Views.PlayerView.add_player_to_db, {}],
-					'2': [cls.add_player_to_db, args],
-					'3': [cls.add_player_to_db, args],
-					'4': [Views.MenuView.main_menu, None],
+					'1': [cls.add_player_to_db, player, value],
+					'2': [cls.add_player_to_db, player, value],
+					'3': Views.MenuView.main_menu,
 				}
-				Util.call_options(options, response)
+				Util.call_options(options, value)
+
+			setattr(player, key, value)
+			step += 1
 		
-		if len(args) == 3:
+		if step == 3:
 
-			if Models.Player.get_from_db(args['last_name'], args['first_name'], args['birth_date']):
+			if Models.Player.get_from_db(player.last_name, player.first_name, player.birth_date):
 				print("Ce joueur existe déjà")
-				args = {}
+				player = Models.Player()
+				step = 0
 
-		Views.PlayerView.add_player_to_db(args)
+		Views.PlayerView.add_player_to_db(player, step)
 
 
 	@staticmethod
@@ -91,8 +104,8 @@ class PlayerController():
 
 	@staticmethod
 	def check_ranking(ranking):
-		if not ranking.isdigit():
+		if not ranking.isdigit() or ranking == '0':
 			print("Classement invalide")
 			return False
 
-		return ranking
+		return int(ranking)
