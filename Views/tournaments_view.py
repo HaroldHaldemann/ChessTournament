96a80667ad6d036c9b179b60268cd94f70b2a0ee
Harold Haldemann <harold.haldemann@gmail.com>
2021-09-14 15:14:36 +0200

    feat: complete load tournament

--- Views/tournaments_view.py
+++ Views/tournaments_view.py
@@ -1,3 +1,4 @@
+import Views
 import Controllers
 
 class TournamentView():
@@ -5,95 +6,76 @@ class TournamentView():
 ##### NEW TOURNAMENT #####
 
 	@classmethod
-	def new_tournament(cls, args):
-		step = len(args)
+	def new_tournament(cls, tournament, step):
 		print("\nCREATION D'UN NOUVEAU TOURNOI")
 
 		options = {
-			1: cls.new_tournament_name,
-			2: cls.new_tournament_place,
-			3: cls.new_tournament_date,
-			4: cls.new_tournament_time_control,
-			5: cls.new_tournament_description,
-			6: cls.new_tournament_number_rounds,
-			7: cls.new_tournament_response,
+			0: cls.new_tournament_name,
+			1: cls.new_tournament_place,
+			2: cls.new_tournament_date,
+			3: cls.new_tournament_time_control,
+			4: cls.new_tournament_description,
+			5: cls.new_tournament_response,
 		}
-		Controllers.Util.call_options(options, step)
-
+		Controllers.Util.call_options(options, step, tournament, step)
 
 	@staticmethod
-	def new_tournament_name(args):
+	def new_tournament_name(tournament, step):
 		name = input("Veuillez renseigner le nom de ce tournoi\n")
 
-		Controllers.TournamentController.check_args(args, name=name)
-
+		Controllers.TournamentController.check_args(tournament, step, name=name)
 
 	@staticmethod
-	def new_tournament_place(args):
+	def new_tournament_place(tournament, step):
 		place = input("Veuillez renseigner le lieu de ce tournoi\n")
 
-		Controllers.TournamentController.check_args(args, place=place)
-
+		Controllers.TournamentController.check_args(tournament, step, place=place)
 
 	@staticmethod
-	def new_tournament_date(args):
+	def new_tournament_date(tournament, step):
 		print("Veuillez renseigner la date de ce tournoi")
 		date = input("Format: AAAA-MM-JJ\n")
 
-		Controllers.TournamentController.check_args(args, date=date)
-
+		Controllers.TournamentController.check_args(tournament, step, date=date)
 
 	@staticmethod
-	def new_tournament_time_control(args):
+	def new_tournament_time_control(tournament, step):
 		print("Veuillez renseigner votre controle du temps")
 		print("1- Bullet")
 		print("2- Blitz")
 		print("3- Coup rapide")
-
 		time_control = input("Veuillez sélectionner une des options disponibles en entrant son numéro\n")
 
-		Controllers.TournamentController.check_args(args, time_control=time_control)
-
+		Controllers.TournamentController.check_args(tournament, step, time_control=time_control)
 
 	@staticmethod
-	def new_tournament_description(args):
+	def new_tournament_description(tournament, step):
 		print("Vous pouvez renseigner des remarques pour ce tournoi")
 		description = input("Laissez vide dans le cas contraire\n")
 
-		Controllers.TournamentController.check_args(args, description=description)
-
-
-	@staticmethod
-	def new_tournament_number_rounds(args):
-		print("Veuillez renseigner le nombre de tours de ce tournoi")
-		number_rounds = input("Nombre entre 1 et 7 compris (Par défault: 4)\n") or 4
-
-		Controllers.TournamentController.check_args(args, number_rounds=number_rounds)
-
+		Controllers.TournamentController.check_args(tournament, step, description=description)
 
 	@staticmethod
-	def new_tournament_response(args):
+	def new_tournament_response(tournament, step):
 		print("Résumé du tournoi")
-		print(f"Nom: {args['name']}")
-		print(f"Lieu: {args['place']}")
-		print(f"Date: {args['date']}")
-		print(f"Gestion du temps: {args['time_control']}")
-		print(f"Remarques: {args['description']}")
-		print(f"Nombre de tours: {args['number_rounds']}")
+		print(f"Nom: {tournament.name}")
+		print(f"Lieu: {tournament.place}")
+		print(f"Date: {tournament.date}")
+		print(f"Gestion du temps: {tournament.time_control}")
+		print(f"Remarques: {tournament.description}")
 
 		print("\nVoulez-vous continuer avec ces paramètres?")
-		print("1- Modifer")
-		print("2- Sauvegarder et continuer")
-
+		print("1- Sauvegarder et continuer")
+		print("2- Sauvegarder et revenir au menu principal")
+		print("3- Annuler et revenir au menu principal")
 		response = input("Veuillez sélectionner une des options disponibles en entrant son numéro\n")
-		Controllers.TournamentController.check_args(args, response=response)
 
+		Controllers.TournamentController.check_args(tournament, step, response=response)
 
 ##### ADD PLAYER TO TOURNAMENT #####
 
-
 	@staticmethod
-	def add_player_to_tournament(all_players, players):
+	def add_player_to_tournament(all_players, players, tournament):
 		print("\nAJOUT DE JOUEURS")
 		print(f"Veuillez ajouter {8-len(players)} joueurs dans le tournoi")
 
@@ -101,69 +83,78 @@ class TournamentView():
 		print("\nListe des joueurs:")
 
 		for index, player in enumerate(all_players):
-			print(f"\n{index + 1}- Nom: {player['last_name']} {player['first_name']}")
-			print(f"Date de naissance: {player['birth_date']}")
-			print(f"Genre: {player['gender']}")
-			print(f"Classement: {player['ranking']}")
-			
+			print(f"\n{index + 1}- Nom: {player.last_name} {player.first_name}")
+			print(f"Date de naissance: {player.birth_date}")
+			print(f"Genre: {player.gender}")
+			print(f"Classement: {player.ranking}")
 		player = input("Veuillez sélectionner une des options disponibles en entrant son numéro\n")
 		players.append(player)
 
-		Controllers.TournamentController.add_player_to_tournament(all_players, players)
-
+		Controllers.TournamentController.add_player_to_tournament(all_players, players, tournament)
 
 	@staticmethod
-	def confirm_players(all_players, players):
+	def confirm_players(all_players, players, tournament):
 		print("\nAJOUT DE JOUEURS")
 		print("\nListe des joueurs:")
 
 		for index, player in enumerate(players):
-			print(f"\n{index + 1}- Nom: {player['last_name']} {player['first_name']}")
-			print(f"Date de naissance: {player['birth_date']}")
-			print(f"Genre: {player['gender']}")
-			print(f"Classement: {player['ranking']}")
+			print(f"\n{index + 1}- Nom: {player.last_name} {player.first_name}")
+			print(f"Date de naissance: {player.birth_date}")
+			print(f"Genre: {player.gender}")
+			print(f"Classement: {player.ranking}")
 
 		print("\nVoulez-vous ces joueurs pour le tournoi?")
 		print("1- Confimer et sauvegarder")
 		print("2- Modifier la séléction")
 		response = input("Veuillez sélectionner une des options disponibles en entrant son numéro\n")
 
-		Controllers.TournamentController.confirm_players(all_players, players, response)
-
+		Controllers.TournamentController.confirm_players(all_players, players, response, tournament)
 
 ##### LOAD TOURNAMENT #####
-
 
 	@staticmethod
 	def load_tournament(list_tournaments):
 		print("\nCHARGEMENT D'UN TOURNOI")
 		print("0- Créer un nouveau tournoi")
+		print("00- Annuler et revenir au menu précédent")
 		print("Liste des tournois:")
 
 		for index, tournament in enumerate(list_tournaments):
 			print(
 				f"{index+1}- " \
-				f"Nom: {tournament['name']}, " \
-				f"Lieu: {tournament['place']}, " \
-				f"Date: {tournament['date']}, " \
-				f"Gestion du temps: {tournament['time_control']}"
+				f"Nom: {tournament.name}, " \
+				f"Lieu: {tournament.place}, " \
+				f"Date: {tournament.date}, " \
+				f"Gestion du temps: {tournament.time_control}"
 			)
 		response = input("Veuillez sélectionner une des options disponibles en entrant son numéro\n")
 
 		Controllers.TournamentController.load_tournament(list_tournaments, response=response)
 
+	@staticmethod
+	def load_step_tournament(tournament):
+		print("\nCHARGEMENT D'UN TOURNOI")
+		print("1- Continuer le tournoi")
+		print("2- Supprimer le tournoi")
+		print("3- Annuler et revenir au menu de chargement")
+		response = input("Veuillez sélectionner une des options disponibles en entrant son numéro\n")
+
+		Controllers.TournamentController.load_step_tournament(tournament, response)
 
 ##### EXPORTS #####
 
+	@staticmethod
+	def export_players():
+		Views.MenuView.export_menu()
 
-	def export_players(self):
-		pass
+	@staticmethod
+	def export_all_tournaments():
+		Views.MenuView.export_menu()
 
-	def export_all_tournaments(self):
-		pass
+	@staticmethod
+	def export_rounds():
+		Views.MenuView.export_menu()
 
-	def export_rounds(self):
-		pass
-
-	def export_matches(self):
-		pass
+	@staticmethod
+	def export_matches():
+		Views.MenuView.export_menu()
