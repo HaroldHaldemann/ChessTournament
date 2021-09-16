3fdf69954d3feb4e149250a7eae10bea5986fb5f
Harold Haldemann <harold.haldemann@gmail.com>
2021-09-16 12:19:15 +0200

    feat: replace match dict with model, better view

--- Views/rounds_view.py
+++ Views/rounds_view.py
@@ -14,7 +14,7 @@ class RoundView:
 
     @staticmethod
     def create_first_round(tournament):
-        print("\nCREATION DES TOURS")
+        print("\nCREATION DU TOUR")
         print("1- Créer un nouveau tour")
         print("2- Revenir au menu principal")
 
@@ -26,21 +26,25 @@ class RoundView:
 
     @staticmethod
     def start_round(tournament, round):
-        print("\nCREATION DES TOURS")
+        print("\nDEBUT DU TOUR")
         print("\nListe des matchs")
         for match in round.matches:
-            p1 = match[0][0]
-            s1 = match[0][1]
-            p2 = match[1][0]
-            s2 = match[1][1]
+            p1 = match.player1
+            s1 = match.score1
+            p2 = match.player2
+            s2 = match.score2
 
-            print(f"\n1- {p1.last_name} {p1.first_name} ({p1.birth_date})")
-            print(f" / Classement: {p1.ranking} / Score: {s1}")
+            print(
+                f"\n1- {p1.last_name} {p1.first_name} ({p1.birth_date})"
+                f" / Classement: {p1.ranking} / Score: {s1}"
+            )
             print("CONTRE")
-            print(f"2- {p2.last_name} {p2.first_name} ({p2.birth_date})")
-            print(f" / Classement: {p2.ranking} / Score: {s2}\n")
+            print(
+                f"2- {p2.last_name} {p2.first_name} ({p2.birth_date})"
+                f" / Classement: {p2.ranking} / Score: {s2}\n"
+            )
 
-        response = input("Veuillez entrer 0 pour commencer le tour\n")
+        response = input("Veuillez entrer 1 pour commencer le tour\n")
         Controllers.RoundController.start_round(
             tournament,
             round,
@@ -49,10 +53,9 @@ class RoundView:
 
     @staticmethod
     def end_round(tournament, round):
-        print("\nCREATION DES TOURS")
-        print("\nMatchs en cours\n")
+        print("\nMATCH EN COURS\n")
 
-        response = input("Veuillez entrer 0 pour terminer le tour\n")
+        response = input("Veuillez entrer 1 pour terminer le tour\n")
         Controllers.RoundController.end_round(
             tournament,
             round,
@@ -62,16 +65,20 @@ class RoundView:
     @staticmethod
     def results_round(tournament, round, step):
         print("\nRESULTAT DU TOUR")
-        p1 = round.matches[step][0][0]
-        s1 = round.matches[step][0][1]
-        p2 = round.matches[step][1][0]
-        s2 = round.matches[step][1][1]
+        p1 = round.matches[step].player1
+        s1 = round.matches[step].score1
+        p2 = round.matches[step].player2
+        s2 = round.matches[step].score2
 
         print("Veuillez determiner le vainqueur")
-        print(f"1- {p1.last_name} {p1.first_name} ({p1.birth_date})")
-        print(f" / Classement: {p1.ranking} / Score: {s1}\n")
-        print(f"2- {p2.last_name} {p2.first_name} ({p2.birth_date})")
-        print(f" / Classement: {p2.ranking} / Score: {s2}\n")
+        print(
+            f"1- {p1.last_name} {p1.first_name} ({p1.birth_date})"
+            f" / Classement: {p1.ranking} / Score: {s1}\n"
+        )
+        print(
+            f"2- {p2.last_name} {p2.first_name} ({p2.birth_date})"
+            f" / Classement: {p2.ranking} / Score: {s2}\n"
+        )
         print("3- Egalité")
 
         response = input("Veuillez entrer le numéro de l'option choisie\n")
@@ -84,19 +91,22 @@ class RoundView:
 
     @staticmethod
     def confirm_round(tournament, round):
-        print("FINALISATION DU ROUND")
+        print("\nFINALISATION DU TOUR\n")
         for match in round.matches:
-            p1 = match[0][0]
-            s1 = match[0][1]
-            p2 = match[1][0]
-            s2 = match[1][1]
+            p1 = match.player1
+            s1 = match.score1
+            p2 = match.player2
+            s2 = match.score2
 
-            print(f"{p1.last_name} {p1.first_name} ")
-            print(f"({p1.birth_date}) / Score: {s1}")
+            print(
+                f"\n{p1.last_name} {p1.first_name} "
+                f"({p1.birth_date}) / Score: {s1}"
+            )
             print("CONTRE")
-            print(f"{p2.last_name} {p2.first_name} ")
-            print(f"({p2.birth_date}) / Score: {s2}")
-
+            print(
+                f"{p2.last_name} {p2.first_name} "
+                f"({p2.birth_date}) / Score: {s2}\n"
+            )
         print("1- Sauvegarder et continuer le tournoi")
         print("2- Sauvegarder et revenir au menu principal")
         print("3- Annuler et revenir au menu principal")
@@ -109,5 +119,34 @@ class RoundView:
         )
 
     @staticmethod
-    def end_tournament(tournament):
-        pass
+    def end_tournament(tournament, winners):
+        print("FIN DU TOURNOI")
+        if len(winners) == 1:
+            player = winners[0][0]
+            score = winners[0][1]
+            print("Le vainqueur est:")
+            print(
+                f"{player.last_name} {player.first_name}"
+                f"({player.birth_date})"
+            )
+        else:
+            print("Les vainqueurs sont:")
+            for winner in winners:
+                player = winner[0]
+                score = winner[1]
+                print(
+                    f"{player.last_name} {player.first_name}"
+                    f"({player.birth_date})"
+                )
+        print(f"Avec un score de: {score}")
+
+        print("Voulez-vous modifier le classement des joueurs?")
+        print("1- Aller au menu de chargement des joueurs")
+        print("2- Revenir au menu principal")
+
+        response = input("Veuillez entrer le numéro de l'option choisie\n")
+        Controllers.RoundController.end_tournament(
+            tournament,
+            winners,
+            response,
+        )
